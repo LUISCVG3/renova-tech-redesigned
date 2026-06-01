4f52e89501f7dba37da406a8e75d0f7c3ec689ff
Luandev <lc5349819@gmail.com>
2026-05-31 19:38:12 -0600

    Update Footer.jsx

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
         <footer className="flex items-center justify-between bg-gray-950 w-full border-t border-gray-900  py-5 px-8">
             <div className="flex flex-col items-center md:items-start gap-4">
                 <div className="text-lg font-black text-white">RenovaTech</div>
-                <p className="font-['Inter'] text-xs uppercase tracking-widest text-gray-500">© 2024 RenovaTech. Solutions for the next generation. BY Luis Angel C. & Emiliano Corderoi</p>
+                <p className="font-['Inter'] text-xs uppercase tracking-widest text-gray-500">© 2024 RenovaTech. Solutions for the next generation. BY Luis Angel C.</p>
             </div>
             <nav className="flex gap-8">
                 <Link href='/services' className="uppercase tracking-widest text-gray-500 hover:text-blue-400 transition-colors">Services</Link>
